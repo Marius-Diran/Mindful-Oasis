e25f6be9c2d962e3ef85b3e810732a2f2592e4dc
themariusodediran@gmail.com <themariusodediran@gmail.com>
2024-08-07 23:16:46 +0100

    Creating a server

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv").config();
+const path = require("path");
 
-const port = 3000;
+let initial_path = path.join(__dirname, "public");
+
+const app = express();
+app.use(express.static(initial_path));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(initial_path, "index.html"));
+})
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
