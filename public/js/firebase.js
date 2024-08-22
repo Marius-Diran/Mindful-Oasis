98b20cc18a81c1a20ec2d550974312660559a002
Marius Odediran <themariusodediran@gmail.com>
2024-08-22 11:03:28 +0100

    Commit

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDAK8vF-R_38TIEzuR1gMuBz7pmKldOEUI",
+  authDomain: "marius-blog-website.firebaseapp.com",
+  projectId: "marius-blog-website",
+  storageBucket: "marius-blog-website.appspot.com",
+  messagingSenderId: "928224624357",
+  appId: "1:928224624357:web:9bed4305820f7c5aefde0c"
+};
+
+
+const app = initializeApp(firebaseConfig);
