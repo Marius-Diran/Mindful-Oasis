5f2f22150112d079da12ae3abae93d3ca45589ab
Marius Odediran <themariusodediran@gmail.com>
2024-08-25 21:26:08 +0100

    Commit

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { 
+  getFirestore, collection, getDocs 
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCfkze9OxYv5rXKjQ0asGpx3iLSLL_wk5k",
@@ -11,8 +12,14 @@ const firebaseConfig = {
   appId: "1:1039381804904:web:6e0d07010395eb6bb488dd"
 };
 
-
+// init firebase
 const app = initializeApp(firebaseConfig);
 
-// init the database
+// init services
 const db = getFirestore(app);
+
+// collecton ref
+const colRef = collection(db, 'blogs');
+
+// get collection data
+getDocs(colRef);
