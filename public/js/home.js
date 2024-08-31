2fcbd08180711d70c79dc63ae19a59bc23ac8f2f
Marius Odediran <themariusodediran@gmail.com>
2024-08-31 12:38:06 +0100

    commit

---
+++ public/js/home.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { 
+  getFirestore, collection, getDocs,
+  addDoc, doc, setDoc
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfkze9OxYv5rXKjQ0asGpx3iLSLL_wk5k",
+  authDomain: "marius-blog-8b621.firebaseapp.com",
+  projectId: "marius-blog-8b621",
+  storageBucket: "marius-blog-8b621.appspot.com",
+  messagingSenderId: "1039381804904",
+  appId: "1:1039381804904:web:6e0d07010395eb6bb488dd"
+};
+
+// init firebase
+const app = initializeApp(firebaseConfig);
+
+// init services
+const firestore = getFirestore(app);
+
+// collecton ref
+const colRef = collection(firestore, 'blogs');
+
+const blogSection = document.querySelector('.first-blog');
+
+getDocs(colRef).then((snapshot) => {
+  snapshot.docs.forEach((doc) => {
+      const blog = { id: doc.id, ...doc.data() };
+      if(blog.id !== decodeURI(location.pathname.split("/").pop())) {
+          createBlog(blog);
+      }
+  });
+});
+
+
+const createBlog = (blog) => {
+  let data = blog.data();
+  blogSection.innerHTML += `
+  <div class="main-story">
+    <div class="main-img_box"><img class="main-img" src="${data.bannerImage}"></div>
+    <button class="inspire-btn">Inspire</button>
+    <h1>${data.title.substring(0, 100) + '...'}</h1>
+    <p>${data.article.substring(0, 1000) + '...'}</p>
+    <a href="/${blog.id}"><button class="fullstory-btn">Full Story <i class="fa-solid fa-arrow-right-long"></i></button></a>
+  </div>
+  `;
+}
